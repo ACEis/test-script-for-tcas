af7f60fb8d3b9b78f5a23543b63efecbd59b6905
ACEis <38037462+ACEis@users.noreply.github.com>
2019-03-28 03:08:20 +0800

    Correct the wrong 'cause LF is not a set value
    LF is not always 65.
    add a timer.

--- newscript.py
+++ newscript.py
@@ -7,7 +7,81 @@ statement = 1
 f = open("./runall.sh")
 oldscript = f.readlines()
 
-for version in range(1,43):
+for version in range(1,42):import os
+import numpy as np
+import time
+time_start=time.time()
+version = 1
+test = 1
+statement = 1
+
+os.system("gcc -O2 ../source.alt/source.orig/tcas.c -o ../source/tcas.exe -w")
+os.system("./runall.sh  > /dev/null")
+f = open("runall.sh")
+oldscript = f.readlines()
+
+for version in range(1,42):
+	LF = 0
+	print('>>>>>>>>running version '+str(version))
+	matrix = np.zeros((1609,100))
+	os.system("gcc -fprofile-arcs -ftest-coverage -o ../versions.alt/versions.orig/v"+str(version)+"/tcas.exe ../versions.alt/versions.orig/v"+str(version)+"/tcas.c -w")
+	os.system("cp ../versions.alt/versions.orig/v"+str(version)+"/tcas.exe ../source")
+	os.system("mv ../scripts/tcas.gcno ../versions.alt/versions.orig/v"+str(version))
+	os.system("mkdir ../newoutputs/v"+str(version)+" -p")
+	oldscript = [line for line in oldscript if(not line.startswith("echo"))]
+	for line in oldscript:#test
+		os.system(line.replace('../outputs', '../newoutputs/v'+str(version)))
+		os.system("mv ../scripts/tcas.gcda ../versions.alt/versions.orig/v"+str(version))
+		os.system("gcov -b ../versions.alt/versions.orig/v"+str(version)+"/tcas.c > /dev/null")
+		g = open("tcas.c.gcov")
+		for line in g.readlines():#statement
+			if(line.strip()[0] in ('1','2','3','4','5','6','7','8')):
+				matrix[test-1][statement-1] = int(line.strip()[0])
+				matrix[-1][statement-1] += 1
+				statement += 1
+			elif(line.strip()[0] == '#'):
+				matrix[test-1][statement-1] = 0
+				statement += 1
+		LF = statement - 1
+		statement = 1
+		test += 1
+		g.close()
+	test = 1
+	np.savetxt("../versions.alt/versions.orig/v"+str(version)+"/matrix",matrix[:,0:LF],fmt='%d')
+	false_num = 0
+	true_num = 0
+	LH = 0
+	max_num = 0
+	min_num = 1608
+	sum_num = 0
+	mean_num = 0
+	for i in range(1, 1609):
+		tmp = os.system("diff ../outputs/t"+str(i)+" ../newoutputs/v"+str(version)+"/t" + str(i) + ">/dev/null")
+		if tmp != 0:
+			false_num += 1
+		else:
+			true_num += 1
+	for i in range(0,LF):
+		if(matrix[-1][i] != 0):
+			LH += 1	
+		if(matrix[-1][i] > max_num):
+			max_num = matrix[-1][i]
+		if(matrix[-1][i] < min_num):
+			min_num = matrix[-1][i]
+		sum_num += matrix[-1][i]
+	linecov = round(LH/LF,4)
+	mean_num = round(sum_num/LF,2)
+	r = open("../versions.alt/versions.orig/v"+str(version)+"/result","w+")
+	r.write("输出正确的用例个数: "+str(true_num)+"\n输出错误的用例个数: "+str(false_num))
+	r.write("\n可执行语句个数: "+str(LF)+"\n被执行语句个数: "+str(LH))
+	r.write("\n语句覆盖率: "+("%.2f%%" % (linecov * 100)))
+	r.write("\n被执行最多次数的语句执行次数: "+str(max_num))
+	r.write("\n被执行最少次数的语句执行次数: "+str(min_num))
+	r.write("\n每个语句平均被执行次数: "+str(mean_num))
+	r.close()
+f.close()
+time_end=time.time()
+print('time cost',time_end-time_start,'s')
 	#version
 	#this module is used for running test language----shell
 	matrix = np.zeros((1609,65))
